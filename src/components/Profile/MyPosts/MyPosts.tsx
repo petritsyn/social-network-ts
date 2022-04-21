32c624f5a029c210d36c4337c081bf6d811115c6
youngvvp <andreyrussia111@gmail.com>
2022-04-21 16:26:53 +0300

    31 - onClick, ref

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -11,8 +11,16 @@ const MyPosts = (props: MyPostsPropsType) => {
 
     const postElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
 
+    let postMessageRef = React.createRef<HTMLTextAreaElement>();
+
+    const addPostHandler = () => {
+        alert(postMessageRef.current?.value)
+    }
+
     return (
         <div className={s.myPosts} >
+            <div><textarea ref={postMessageRef}/></div>
+            <div><button onClick={addPostHandler}>Add post</button></div>
             {postElements}
         </div>
     )
